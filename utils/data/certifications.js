e9d0d3f64c21321aa05f7a6be871f87eaf16c34a
Aviral Saxena <aviral.saxena.11.70@gmail.com>
2026-08-14 19:51:38 +0530

    Update certification years to 2026 and reorder by year

--- utils/data/certifications.js
+++ utils/data/certifications.js
@@ -1,30 +1,30 @@
 export const certifications = [
   {
     id: 1,
+    title: "Career Edge - AI Foundation",
+    issuer: "TCS iON",
+    duration: "2026",
+    credential: "https://drive.google.com/file/d/1auIVwKWbYXP33qUvLMFqMT7_iCw3fPbm/view?usp=sharing"
+  },
+  {
+    id: 2,
+    title: "Agentic AI Certified Foundations Associate",
+    issuer: "Oracle",
+    duration: "2026",
+    credential: "https://drive.google.com/file/d/1K9V2_XpdzZ7RcBzlj_kc1dWpxAJ2_Yie/view?usp=sharing"
+  },
+  {
+    id: 3,
     title: "Google Cloud Computing Foundations",
     issuer: "NPTEL",
     duration: "2025",
     credential: "https://drive.google.com/file/d/1hckVUbZhysKb5MxUDGMrPxKxf1sgvV_m/view?usp=sharing"
   },
   {
-    id: 2,
+    id: 4,
     title: "Cloud Computing and Distributed Systems",
     issuer: "NPTEL",
     duration: "2025",
     credential: "https://drive.google.com/file/d/13677tWMzG7xV_Cnj6ZVqHH0xIGkB2RcU/view?usp=sharing"
-  },
-  {
-    id: 3,
-    title: "Career Edge - AI Foundation",
-    issuer: "TCS iON",
-    duration: "2025",
-    credential: "https://drive.google.com/file/d/1auIVwKWbYXP33qUvLMFqMT7_iCw3fPbm/view?usp=sharing"
-  },
-  {
-    id: 4,
-    title: "Agentic AI Certified Foundations Associate",
-    issuer: "Oracle",
-    duration: "2025",
-    credential: "https://drive.google.com/file/d/1K9V2_XpdzZ7RcBzlj_kc1dWpxAJ2_Yie/view?usp=sharing"
   }
 ]
